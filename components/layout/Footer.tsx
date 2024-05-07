a06f2f3e2d60df77b2d62b8e1fd8dbe9e0d39505
minh le <117999133+sixxson@users.noreply.github.com>
2024-05-07 15:16:20 +0700

    01-Craete-Layout-Next-Shop

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className='footer footer-center p-4 bg-base-300 text-base-content'>
+            <div>
+                <p>&copy; 2021 Fashion Brand</p>
+            </div>
+        </footer>
+    )
+}
---
+++ components/layout/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+export default function Header() {
+    return (
+        <div>
+
+            <nav className='navbar justify-between bg-base-300'>
+                <ul>
+                    <li>
+                        <Link href='/' className='btn btn-ghost'>FashtionBrand</Link>
+                    </li>
+                </ul>
+                <ul className="sm:flex sm:justify-center sm:items-center mt-4">
+                    <div className="flex flex-col sm:flex-row">
+                        <Link className="mt-3 text-base-content hover:underline sm:mx-3 sm:mt-0" href="/">Home</Link>
+                        <li className="dropdown dropdown-hover">
+                            <Link tabIndex={0} role="button" className="mt-3 text-base-content hover:underline sm:mx-3 sm:mt-0" href="/product">Shop</Link>
+                            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 left-[-100%] shadow bg-base-100 rounded-box w-52">
+                                <li><Link href=''>Item 1</Link></li>
+                                <li><Link href=''>Item 2</Link></li>
+                            </ul>
+                        </li>
+                        <Link className="mt-3 text-base-content hover:underline sm:mx-3 sm:mt-0" href="#">Categories</Link>
+                        <Link className="mt-3 text-base-content hover:underline sm:mx-3 sm:mt-0" href="#">Contact</Link>
+                        <Link className="mt-3 text-base-content hover:underline sm:mx-3 sm:mt-0" href="#">About</Link>
+                    </div>
+                </ul>
+                <ul className='flex-2'>
+                    <li>
+                        <Link href='' className=' btn btn-ghost rounded-btn '> Crat</Link>
+                    </li>
+                    <li>
+                        <Link href='' className=' btn btn-ghost rounded-btn '> Sign in</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
